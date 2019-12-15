bab8ed0f0598e42329856ecc1280c2ef91a240ef
jmurrayrobertson <58891335+jmurrayrobertson@users.noreply.github.com>
2019-12-14 23:04:01 -0600

    Add files via upload

---
+++ QuestionsCh1.py
@@ -0,0 +1,43 @@
+# Q1: Which of the following are operators, and which are values?
+    # *       : operator for both math and string operations
+    # 'print' : value
+    # -88.8   : value
+    # -       : math operator
+    # /       : math operator
+    # +       : operator for both math and string operations
+    # 5       : value
+
+# Q2: Which of the following is a variable, and which is a string?
+    # spam    : variable
+    # 'spam'  : string
+
+# Q3: Name three data types.
+    # strings, integers, and floating-point numbers
+
+# Q4: What is an expression made up of? What do all expressions do?
+    # values and operators. Reduce down to a single value
+
+# Q5: 
+
+# Q6: What does the variable bacon contain after the following code runs?
+#       bacon = 20
+#       bacon + 1
+    # the variable bacon would contain 21
+
+# Q7: What should the following two expressions evaluate to?
+    #   'spam' + 'spamspam'
+        #   'spamspamspam'
+    #   'spam' * 3
+        #   'spamspamspam'
+
+# Q8: Why is eggs a valid variable name while 100 is invalid?
+    # because eggs starts with a letter but 100 starts with a number
+
+# Q9: What three functions can be used to get the integer, floating-point
+#     number, or string version of a value?
+    # int(), float(), str()
+
+# Q10: Why does this expression cause an error? How can you fix it?
+#       'I have eaten ' + 99 + ' burritos.'
+    # because the expression is trying to combine an integer with strings.
+    # to fix it, use the str() function on the number 99 to make it a string.
